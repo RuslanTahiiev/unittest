8079ed6f495194466395d3e5022d6b83219a7162
Ruslan <tagiyevazeandua@gmail.com>
2021-11-05 15:27:00 +0200

    pytest#2

--- pytest_ex/conftest.py
+++ pytest_ex/conftest.py
@@ -1,12 +1,4 @@
 import pytest
-from params_test import FILENAME
 
 
-@pytest.fixture(autouse=True)
-def clean_file(filename=FILENAME):
-    try:
-        with open(filename, 'w'):
-            pass
-    except Exception() as e:
-        print(e)
-
+pytest_plugins = ['file_fixtures']
